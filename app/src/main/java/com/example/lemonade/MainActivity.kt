95e61e67fec26ad6d2f8bb00ce32006139eb9ccd
hyper <hyperskill@hyperskill.org>
2023-03-03 22:26:58 +0800

    button

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,19 +56,24 @@ fun LemonadeWithButtonAndImage(modifier: Modifier = Modifier
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = stringResource(descriptions[currentImageIndex]), fontSize = 18.sp)
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            if (currentImageIndex == 1 && clickCount != clickThreshold) {
-                clickCount += 1
-            } else {
-                currentImageIndex = (currentImageIndex + 1) % images.size
-                clickCount = 0
-            }
-        }) {
-            Image(
-                painter = painterResource(id = images[currentImageIndex]),
-                contentDescription = null
-            )
-        }
+        Image(
+            painter = painterResource(id = images[currentImageIndex]),
+            contentDescription = null,
+            modifier = Modifier
+                .border(
+                    width = 1.dp,
+                    color = Color(105, 205, 216),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .clickable(onClick = {
+                    if (currentImageIndex == 1 && clickCount != clickThreshold) {
+                        clickCount += 1
+                    } else {
+                        currentImageIndex = (currentImageIndex + 1) % images.size
+                        clickCount = 0
+                    }
+                })
+        )
     }
 }
 
